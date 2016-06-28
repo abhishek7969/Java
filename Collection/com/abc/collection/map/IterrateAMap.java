8b2c73ac16b4a276329ff21f8e3be6ec994db39b
Abhishek kumar <abhishek7969@gmail.com>
2016-06-29 00:01:03 +0530

    adding format

--- Collection/com/abc/collection/map/IterrateAMap.java
+++ Collection/com/abc/collection/map/IterrateAMap.java
@@ -1,16 +1,18 @@
 package com.abc.collection.map;
-import java.util.*;
-class TestCollection13{
- public static void main(String args[]){
- 
-HashMap<Integer,String>  hm = new HashMap<Integer,String>();
-hm.put(100,"Amit");
-hm.put(101,"kumar");
-hm.put(102,"rai");
-hm.put(102,"manish");
 
-	for(Map.Entry entry : hm.entrySet()){
-	System.out.println("Key ="+entry.getKey()+ " and value = "+entry.getValue());
+import java.util.*;
+
+class TestCollection13 {
+	public static void main(String args[]) {
+
+		HashMap<Integer, String> hm = new HashMap<Integer, String>();
+		hm.put(100, "Amit");
+		hm.put(101, "kumar");
+		hm.put(102, "rai");
+		hm.put(102, "manish");
+
+		for (Map.Entry entry : hm.entrySet()) {
+			System.out.println("Key =" + entry.getKey() + " and value = " + entry.getValue());
+		}
 	}
-					}
 }
