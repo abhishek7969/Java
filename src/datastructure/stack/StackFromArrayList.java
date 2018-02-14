e915f742aef738a670982dde07aff9b2a623ca27
Abhishek kumar <abhishek769@gmail.com>
2018-02-14 17:39:57 +0530

    new stack changes

--- src/datastructure/stack/StackFromArrayList.java
+++ src/datastructure/stack/StackFromArrayList.java
@@ -6,34 +6,30 @@ import java.util.List;
 public class StackFromArrayList {
 	private int max;
 	private List<Integer> list ;
-	private int top;
 	
 	public StackFromArrayList(int size){
 		max = size;
 		list = new ArrayList<Integer>(max);
-		top = -1;
 	}
 	
 	public void push(int item){
 		list.add(item);
-		top++;
 	}
 	public int pop(){
-		Integer i =  (Integer) list.get(top);
-		top = top-1;
+		Integer i =  (Integer) list.get(list.size()-1);
 		return i;
 	}
 	public boolean isEmpty(){
-		if(top == -1)
+		if(list.size() == 0)
 			return true;
 		else
 			return false;
 	}
 	public int peek(){
-		return list.get(top);
+		return list.get(list.size()-1);
 	}
 	public boolean isFull(){
-		if(top == max-1)
+		if(list.size() == max)
 			return true;
 		else
 			return false;
@@ -42,15 +38,15 @@ public class StackFromArrayList {
 	public static void main(String[] args) {
 		StackFromArrayList sfa = new StackFromArrayList(5);
 		System.out.println(sfa.isEmpty());
-		sfa.push(12);
-		sfa.push(122);
+		sfa.push(1);
+		sfa.push(2);
+		sfa.push(3);
+		sfa.push(4);
+		sfa.push(5);
 		
-		sfa.pop();
 		System.out.println(sfa.pop());
-		System.out.println(sfa.pop());
-		System.out.println(sfa.pop());
-
-		System.out.println(sfa.peek());
+		
+		System.out.println(sfa.isFull());
 	}
 	
 }
